af51df75be017a05af893a3360504b1548b62e6f
pf4 <74263128+elydre@users.noreply.github.com>
2025-07-24 14:14:46 +0200

    Update getcover.py

--- projet/music/getcover.py
+++ projet/music/getcover.py
@@ -7,8 +7,16 @@
 import urllib.parse
 import requests
 import sys
+import os
 
 directories = sys.argv[1:]
+
+if not directories:
+    print("NOTE:    path used as album name, write album name as argument for fine searching")
+    print("Usage:   python getcover.py [first album name] [second album name] [...]")
+    print("->       python getcover.py 'Lana Del Rey - Blue Banisters'\n")
+
+    directories = [os.path.basename(os.getcwd())]
 
 for d in directories:
     url = "https://music.apple.com/fr/search?term=" + urllib.parse.quote(d)
@@ -28,7 +36,7 @@ for d in directories:
 
         print(f"{d} [{len(response.content) / 1024 / 1024:.2f} MB in {fmt.upper()}]")
 
-        with open(f"cover.{fmt}", "wb") as f:
+        with open(f"{d}.{fmt}", "wb") as f:
             f.write(response.content)
     except Exception as e:
         print(f"{d} [Error: {e}]")
