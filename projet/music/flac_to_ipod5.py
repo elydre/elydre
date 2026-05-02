3495662b664d61cdcc11b53623a2b513b2a3cda4
pf4 <74263128+elydre@users.noreply.github.com>
2026-05-02 14:37:05 +0200

    Update flac_to_ipod5.py

--- projet/music/flac_to_ipod5.py
+++ projet/music/flac_to_ipod5.py
@@ -1,4 +1,4 @@
-from mutagen.mp4 import MP4, MP4Cover
+from mutagen.flac import FLAC, Picture
 from PIL import Image
 import subprocess
 import threading
@@ -7,39 +7,64 @@ import sys
 import os
 import io
 
-
 T_COUNT = 0
 M_COUNT = 28
 
 COMPLETED = 0
 TOTAL = 0
 
+OUTFILES = []
+
+def tofilename(s):
+    t = '’'
+
+    for r in [["\xa0", " "], ["…", ""], ["“", ""], ["`", t], ["´", t], ["–", "-"], ["?", ""],
+              ["—", "-"], ["/", "-"], [":", ""], ["\"", ""], ["'", t], [".", ""], ["\\", ""]]:
+        s = s.replace(r[0], r[1])
+
+    while "  " in s:
+        s = s.replace("  ", " ")
+
+    return s.strip()
+
 
 def update_cover(path, album_cover):
-    audio = MP4(path)
+    audio = FLAC(path)
 
+    # remove all pictures
+    audio.clear_pictures()
+
+    # create a new picture from the album cover in 600x600
     img = Image.open(album_cover)
     img = img.convert("RGB")
     img = img.resize((500, 500), Image.LANCZOS)
     img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format="JPEG", quality=90)
-    img_data = img_byte_arr.getvalue()
+    img.save(img_byte_arr, format="JPEG", quality=95)
+    img_byte_arr.seek(0)
 
-    # Ajoute la nouvelle couverture
-    cover = MP4Cover(img_data, imageformat=MP4Cover.FORMAT_PNG)
-    audio["covr"] = [cover]
+    # add the new picture to the audio file
+    picture = Picture()
+    picture.data = img_byte_arr.read()
+    picture.type = 3 # front cover
+    picture.mime = "image/jpeg"
+    picture.desc = ""
+    picture.width  = 500
+    picture.height = 500
+    audio.add_picture(picture)
 
     audio.save()
+    
 
-
-def convert_flac_to_alac(input_file, output_file):
+def convert_flac_to_flac16(input_file, output_dir):
     """
-    input_file: path to input FLAC file
+    input FLAC file
     -> 16 / 24bit lossless 44.1 / 48kHz or more
 
-    output_file: path to output M4A file
+    output FLAC file
     -> 16bit lossless 44.1 / 48kHz ONLY
     """
+
+    global T_COUNT, COMPLETED
 
     for name in ["cover.jpg", "cover.png"]:
         path = os.path.join(os.path.dirname(input_file), name)
@@ -48,6 +73,21 @@ def convert_flac_to_alac(input_file, output_file):
     else:
         print(f"No cover file found for {input_file}!")
         os._exit(1)
+
+    in_audio = FLAC(input_file)
+
+    album_artist = in_audio.get("albumartist", [in_audio.get("artist", ["Unknown"])[0]])[0]
+    album_name   = in_audio.get("album", ["Unknown"])[0]
+    track_number = in_audio.get("tracknumber", ["0"])[0]
+    title        = in_audio.get("title", ["Unknown"])[0]
+
+    output_file = os.path.join(output_dir, tofilename(album_artist), tofilename(album_name), f"{track_number}. {tofilename(title)}.flac")
+    OUTFILES.append(output_file)
+
+    if os.path.exists(output_file):
+        T_COUNT -= 1
+        COMPLETED += 1
+        return
 
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
@@ -74,7 +114,7 @@ def convert_flac_to_alac(input_file, output_file):
         print(f"Unsupported sample rate {input_srate} Hz in {input_file}!")
         os._exit(1)
 
-    print(f"IN: {input_srate} Hz -> OUT: {output_srate} Hz")
+    # print(f"IN: {input_srate} Hz -> OUT: {output_srate} Hz")
 
     if (subprocess.run([
         "ffmpeg",
@@ -84,7 +124,8 @@ def convert_flac_to_alac(input_file, output_file):
         "-vn",
         "-map_metadata", "0",
         "-af", f"aresample={output_srate}:resampler=soxr:precision=28,aformat=sample_fmts=s16",
-        "-c:a", "alac",
+        "-c:a", "flac",
+        "-compression_level", "5",
         output_file
     ]).returncode != 0):
         print(f"Error converting {input_file} to ALAC!")
@@ -92,24 +133,11 @@ def convert_flac_to_alac(input_file, output_file):
 
     update_cover(output_file, path)
 
-    global T_COUNT, COMPLETED
     T_COUNT -= 1
     COMPLETED += 1
 
     print(f"{COMPLETED/TOTAL * 100:.2f}% - {output_file.split('/')[-1]}")
 
-def read_album_list(file_path):
-    if not os.path.exists(file_path):
-        return []
-
-    with open(file_path, 'r') as f:
-        return [line.strip() for line in f if line.strip()]
-
-def get_allout(output_root):
-    if not os.path.isdir(output_root):
-        return []
-
-    return [album for album in os.listdir(output_root) if os.path.isdir(os.path.join(output_root, album))]
 
 def should_ignore(album, album_list):
     if album in album_list:
@@ -121,65 +149,74 @@ def should_ignore(album, album_list):
 
     return True
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python flac_to_ipod5.py <input_dir> <output_root>")
         sys.exit(1)
 
+    input_root  = sys.argv[1]
     output_root = sys.argv[2]
-    root = sys.argv[1]
 
-    album_list = read_album_list("ipod5_list.txt")
-
-    allout = get_allout(output_root)
-    todo = []
-
-
-    if not os.path.isdir(root):
-        print(f"Error: {root} is not a directory.")
+    if not os.path.exists("ipod5_list.txt"):
+        print("error: ipod5_list.txt not found!")
         sys.exit(1)
 
-    for album in os.listdir(root):
-        if not os.path.isdir(os.path.join(root, album)):
+    with open("ipod5_list.txt", 'r') as f:
+        album_list = [line.strip() for line in f if line.strip()]
+
+    todo = []
+
+    if not os.path.isdir(input_root):
+        print(f"Error: {input_root} is not a directory.")
+        sys.exit(1)
+
+    for album in os.listdir(input_root):
+        if not os.path.isdir(os.path.join(input_root, album)):
             continue
 
         if should_ignore(album, album_list):
             continue
 
-        for file in os.listdir(os.path.join(root, album)):
-            filepath = os.path.join(root, album, file)
+        for file in os.listdir(os.path.join(input_root, album)):
+            filepath = os.path.join(input_root, album, file)
 
             if not (os.path.isfile(filepath) and filepath.lower().endswith(".flac")):
                 continue
 
-            outpt = os.path.join(output_root, album, f"{os.path.splitext(file)[0]}.m4a")
-
-            if os.path.exists(outpt):
-                print(f"OK  - {outpt.split('/')[-1]}")
-            else:
-                todo.append((filepath, outpt))
+            todo.append(filepath)
 
     TOTAL = len(todo)
 
-    for input_file, output_file in todo:
+    for input_file in todo:
         T_COUNT += 1
         while T_COUNT >= M_COUNT:
             time.sleep(0.1)
-        threading.Thread(target=convert_flac_to_alac, args=(input_file, output_file)).start()
+        threading.Thread(target=convert_flac_to_flac16, args=(input_file, output_root)).start()
 
     while T_COUNT > 0:
         time.sleep(0.1)
 
     os.system("stty sane")
 
-    print("All conversions completed.")
+    # list all output files and remove those that are not in OUTFILES
+    for root, dirs, files in os.walk(output_root):
+        for file in files:
+            path = os.path.join(root, file)
+            if path in OUTFILES:
+                continue
+            print(f"do you want to remove {path}? (Y/n)")
+            if input() in ["Y", "y", ""]:
+                os.remove(path)
+        
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            if os.listdir(dir_path):
+                continue
+            print(f"do you want to remove empty directory {dir_path}? (Y/n)")
+            if input() in ["Y", "y", ""]:
+                os.rmdir(dir_path)
 
-    for album in allout:
-        if should_ignore(album, album_list):
-            dirpath = os.path.join(output_root, album)
-            print(f"'{album}' found in output but not in input")
-            if input("Delete it? (y/n) ").strip().lower() == 'y':
-                print(f"Deleting {dirpath}")
-                os.system(f"rm -rf '{dirpath}'")
+    print("All conversions completed.")
 
     os._exit(0)
